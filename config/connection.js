8bf58ebcc4776817cc1a3e88bee97bb0f7e177e6
Steven Mann <stevencmann2@gmail.com>
2020-01-06 12:17:23 -0600

    attempting to configure orm.js with mySQL; will need to verify

--- config/connection.js
+++ config/connection.js
@@ -8,9 +8,13 @@ const connection = mysql.createConnection({
     password: "phish",
     database: "burgers_db"
   });
-  connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected!');
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+  
+    console.log("connected as id " + connection.threadId);
   });
 
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,12 +1,31 @@
 const connection = require('./connection.js') ;
 
-
+const orm = {
 //selectAll()
-
+selectAll: function(table) {
+    const queryString = "SELECT * FROM ??";
+    connection.query(queryString, [table], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
 //insertOne()
-
+insertOne: function(table, tableCol, valOfCol) {
+    const queryString = "INSERT INTO ?? ?? VALUES ?";
+    connection.query(queryString, [table, tableCol, valOfCol], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
 //updateOne()
+updateOne: function(table, tableCol, valOfCol, tableColtwo, valofColtwo) {
+    const queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+    connection.query(queryString, [table, tableCol, valOfCol, tableColtwo, valofColtwo], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
 
 
-
-module.exports = ORM; 
+}
+module.exports = orm; 
