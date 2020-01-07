d46e28a44cce39b46c20f4abc19bc330769f3f94
Steven Mann <stevencmann2@gmail.com>
2020-01-07 10:00:58 -0600

    use routes in server.js; developing handlebars layouts in main and index

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require("express");
+var exphbs = require("express-handlebars");
 const app = express();
+
 
 // Set port of app
 // process.env.PORT lets the port be set by Heroku
 const PORT = process.env.PORT || 8080;
 
 // Parse request body as JSON
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+//Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
 
 // const connection = mysql.createConnection({
 //     host: "localhost",
