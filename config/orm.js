3f342f7e5afca5b1da3f2d2898422afce3668d53
Steven Mann <stevencmann2@gmail.com>
2020-01-07 15:24:01 -0600

    discovered bug; assignment to constant variable; changed to let; burger + succesful

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,4 @@
-const connection = require('./connection.js') ;
+const connection = require('./connection.js');
 
 // prints questions to mySQL syntax
 function printQuestionMarks(num) {
@@ -34,17 +34,17 @@ function objToSql(ob) {
 
 // Object for all our SQL statement functions.
 const orm = {
-  selectAll: function(tableInput, cb) {
+  selectAll: function (tableInput, cb) {
     const queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
+    connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
       }
       cb(result);
     });
   },
-  insertOne: function(table, cols, vals, cb) {
-    const queryString = "INSERT INTO " + table;
+  insertOne: function (table, cols, vals, cb) {
+    let queryString = "INSERT INTO " + table;
 
     queryString += " (";
     queryString += cols.toString();
@@ -55,7 +55,7 @@ const orm = {
 
     console.log(queryString);
 
-    connection.query(queryString, vals, function(err, result) {
+    connection.query(queryString, vals, function (err, result) {
       if (err) {
         throw err;
       }
@@ -63,9 +63,9 @@ const orm = {
       cb(result);
     });
   },
- 
-  updateOne: function(table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
+
+  updateOne: function (table, objColVals, condition, cb) {
+    let queryString = "UPDATE " + table;
 
     queryString += " SET ";
     queryString += objToSql(objColVals);
@@ -73,7 +73,7 @@ const orm = {
     queryString += condition;
 
     console.log(queryString);
-    connection.query(queryString, function(err, result) {
+    connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
       }
@@ -84,4 +84,4 @@ const orm = {
 };
 
 
-module.exports = orm; 
+module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,7 +14,7 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-  burger.selectOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) { /////////MAKE SURE DEVOUR PERTAINS TO BODY
+  burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) { /////////MAKE SURE DEVOUR PERTAINS TO BODY
     // Send back the ID of the new quote
     res.json({
       id: result.insertId
