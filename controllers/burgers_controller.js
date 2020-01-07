0c3cbf767e706afe3f71487372760200b6c447fc
Steven Mann <stevencmann2@gmail.com>
2020-01-07 15:14:46 -0600

    depbugging js; related to adding new burger

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,44 +3,45 @@ const router = express.Router();
 const burger = require('../models/burger.js');
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
-      const hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    const hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function (req, res) {
+  burger.selectOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) { /////////MAKE SURE DEVOUR PERTAINS TO BODY
+    // Send back the ID of the new quote
+    res.json({
+      id: result.insertId
     });
   });
-  
-  router.post("/api/burgers", function(req, res) {
-    burger.selectOne(["name", "sleepy"], [req.body.name, req.body.devour], function(result) {   /////////MAKE SURE DEVOUR PERTAINS TO BODY
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
-    });
-  });
-  
-  router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-  
-    console.log("condition", condition);
-  
-    burger.updateOne(
-      {
-        devoured: req.body.devour///////// MAKE SURE THIS PERTAINS TO BODY
-      },
-      condition,
-      function(result) {
-        if (result.changedRows === 0) {
-          // If no rows were changed, then the ID must not exist, so 404
-          return res.status(404).end();
-        }
-        res.status(200).end();
-  
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+      devoured: req.body.devoured ///////// MAKE SURE THIS PERTAINS TO BODY
+    },
+    condition,
+    function (result) {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
       }
-    );
-  });
-  
+      res.status(200).end();
+
+    }
+  );
+});
+
 
 
 
