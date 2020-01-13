26734079c4400a1290923bedf9c7937ddcc51424
Steven Mann <stevencmann2@gmail.com>
2020-01-13 11:41:55 -0600

    installing packages; editing schema and burgers.js setting models

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,4 @@
+-- Drops the todolist if it exists currently --
 DROP DATABASE IF EXISTS burgers_db;
-
+-- Creates the "todolist" database --
 CREATE DATABASE burgers_db;
-
-USE burgers_db;
-
-CREATE TABLE burgers (
-    id int NOT NULL AUTO_INCREMENT, 
-    burger_name varchar(100) NOT NULL, 
-    devoured BOOLEAN default false,
-    PRIMARY KEY(id)
-)
--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,18 @@
-const orm = require('../config/orm.js');
-
-const burger = {
-    selectAll: function (cb) {
-        orm.selectAll("burgers", function (res) {
-            cb(res);
-        });
-    },
-   
-    insertOne: function (cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function (res) {
-            cb(res);
-        });
-    },
-    updateOne: function (objColVals, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function (res) {
-            cb(res);
-        });
-    }
-};
-
-
-module.exports = burger;
+module.exports = function(sequelize, DataTypes) {
+    const Burger = sequelize.define("burger", {
+      burger_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 155]
+        }
+      },
+      devoured: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      }
+    });
+    return Burger;
+  };
+  
+  // module.exports = burger;
